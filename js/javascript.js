683e32dade71d49a12e7f47c8dc7a9866cd9cca6
khael6690 <umamjr007@gmail.com>
2023-06-05 11:56:05 +0700

    first

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let x = document.getElementById("myMenu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
